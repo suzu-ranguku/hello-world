caa0e2bb6cb79ccc615ce6a15e5d30f258767e00
Zuzana Hubačová <suzu.ranguru@gmail.com>
2021-04-29 18:55:40 +0200

    hubba bubba

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { Link } from "gatsby"
+import Header from "../components/header"
+
+export default function About() {
+  return (
+    <div style={{ color: `teal` }}>
+      <Header headerText="About Gatsby" />
+      <Header headerText="It's pretty cool" />
+      <p>Such wow. Very React.</p>
+      <Link to="/">Home</Link>
+    </div>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,14 @@
 import React from "react"
+import { Link } from "gatsby"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div style={{ color: `purple` }}>
+      <Link to="/contact/">Contact</Link>
+      <h1>Hello Gatsby!</h1>
+      <p>What a world!</p>
+      <Link to="/about">About</Link>
+      <img src="https://source.unsplash.com/random/400x200" alt="" />
+    </div>
+  )
 }
